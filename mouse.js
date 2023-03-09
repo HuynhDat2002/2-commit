ee00bffe16a6d5c36d990cd05424e8ba6ecfd9b0
HuynhDat2002 <hltdat2002@gmail.com>
2023-03-09 21:19:08 +0700

    add mouse file

---
+++ mouse.js
@@ -0,0 +1,20 @@
+
+function Mouse(name){
+    this.name=name;
+    this.hungry=true;
+}
+Mouse.prototype.eat=function(){
+    this.hungry=false;
+};
+let mouse=new Mouse('mickey');
+console.log(mouse);
+mouse.eat();
+console.log(mouse);
+
+let mouse2=new Mouse('jerry');
+// import chalk from "chalk";
+// Mouse.prototype.sayHi=function(){
+
+//     console.log('hi. ' +chalk.green('im Dat'));
+// }
+module.exports=Mouse;
